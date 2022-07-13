aa12b3759ebc53402d85b9a6df0b41fffef8ad47
Marc McCarthy <marstheory20@gmail.com>
2022-07-13 16:50:08 -0500

    Added delete feature with regular table.

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -7,6 +7,7 @@ import vehicleMakesSaga from './vehicleMakes.saga';
 import vehicleYearsSaga from './vehicleYears.saga';
 import vehicleModelsSaga from './vehicleModels.saga';
 import getTripsSaga from './getTrips.saga';
+import deleteTripSaga from './deleteTrip.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -25,5 +26,6 @@ export default function* rootSaga() {
         vehicleYearsSaga(),
         vehicleModelsSaga(),
         getTripsSaga(),
+        deleteTripSaga(),
 	]);
 }
---
+++ src/redux/sagas/deleteTrip.saga.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+import getTripsSaga from './getTrips.saga';
+
+// Worker Saga: will be fired on "DELETE_TRIP_SAGA" actions
+function* deleteTrip(action) {
+    console.log('DELETE: ACTION.PAYLOAD', action.payload);
+	try {
+        yield axios.delete(`/api/trips/${action.payload}`)
+            .then(response => {
+                console.log('DELETE TRIP RESPONSE', response)
+            })
+            .catch(error => {
+                console.log('DELETE TRIP ERROR', error)
+            })
+        yield put({ type: 'GET_TRIPS_SAGA' });
+
+	} catch (error) {
+		console.log('Error in deleteTrip.saga:', error);
+	}
+}
+
+function* deleteTripSaga() {
+	yield takeLatest('DELETE_TRIP_SAGA', deleteTrip);
+}
+
+export default deleteTripSaga;
