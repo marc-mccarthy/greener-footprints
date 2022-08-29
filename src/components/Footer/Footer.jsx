5d33219d08ee44d3f88ec4611e43d8b072be3406
Marc McCarthy <marstheory20@gmail.com>
2022-08-28 20:17:39 -0500

    Added year to footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,24 @@
 import {Box, Stack} from '@mui/material';
 const footprint=require('../../images/carbon-footprint.png');
 
+let year = new Date().getFullYear();
+
 function Footer() {
     return (
-        <Stack className='footer' spacing={3} direction='row' justifyContent='center'>
-            <img src={footprint} height='25px' alt='footprint' />
+        <Stack
+            className="footer"
+            spacing={3}
+            direction="row"
+            justifyContent="center"
+        >
+            <img src={footprint} height="25px" alt="footprint" />
             <p>
-                &copy; Greener Foot(prints): Road Trip Edition by Marc McCarthy
+                &copy; {year} - Greener Foot(prints): Road Trip
+                Edition by Marc McCarthy
             </p>
-            <img src={footprint} height='25px' alt='footprint' />
+            <img src={footprint} height="25px" alt="footprint" />
         </Stack>
-    )
+    );
 }
 
 export default Footer;
