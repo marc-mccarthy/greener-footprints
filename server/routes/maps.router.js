0b64c8aff9e326cc9f316487921a3f9316d712b9
Marc McCarthy <marstheory20@gmail.com>
2023-02-08 11:22:31 -0600

    Updated key reference.

--- server/routes/maps.router.js
+++ server/routes/maps.router.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-require('dotenv').config();
-const axios = require('axios');
+require("dotenv").config();
+const axios = require("axios");
 
 // POST maps
-router.post('/', (req, res) => {
-    // console.log('MAPS API SERVER req.body:', req.body.directionsUrl + process.env.GOOGLE_MAPS_KEY);
-    axios
-		.get(req.body.directionsUrl + process.env.GOOGLE_MAPS_KEY)
-		.then(response => {
+router.post("/", (req, res) => {
+	// console.log('MAPS API SERVER req.body:', req.body.directionsUrl + process.env.GOOGLE_MAPS_KEY);
+	axios
+		.get(req.body.directionsUrl + process.env.REACT_APP_GOOGLE_MAPS_KEY)
+		.then((response) => {
 			// console.log('MAPS API SERVER response:', response.data.routes[0].legs[0]);
 			res.send(response.data.routes[0].legs[0]);
 		})
-		.catch(error => {
-			console.log('Error in POST /api/trips/maps:', error);
+		.catch((error) => {
+			console.log("Error in POST /api/trips/maps:", error);
 			res.sendStatus(500);
 		});
 });
